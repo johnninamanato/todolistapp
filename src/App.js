2fad98c2b77fae9b0041cfd01ee97331ddd6fc3c
johnninamanato <144648711+johnninamanato@users.noreply.github.com>
2023-12-29 00:52:15 +0800

    accomplished a to do list app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [editedTaskValue, setEditedTaskValue] = useState("");
+
+  function addTask() {
+    if (!newTask) {
+      alert("Enter an Item.");
+      return;
+    }
+
+    const task = {
+      id: Math.floor(Math.random() * 1000),
+      value: newTask,
+    };
+
+    setTasks((oldList) => [...oldList, task]);
+    setNewTask("");
+  }
+
+  function deleteTask(id) {
+    const newArray = tasks.filter((task) => task.id !== id);
+    setTasks(newArray);
+  }
+
+  function startEditingTask(id, value) {
+    setEditingTaskId(id);
+    setEditedTaskValue(value);
+  }
+
+  function saveEditedTask() {
+    setTasks((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === editingTaskId ? { ...task, value: editedTaskValue } : task
+      )
+    );
+    setEditingTaskId(null);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My To-Do List </h1>
+
+      <input
+        type="text"
+        placeholder="Enter tasks"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+
+      <button onClick={addTask}> Add </button>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {editingTaskId === task.id ? (
+              <>
+                <input
+                  type="text"
+                  value={editedTaskValue}
+                  onChange={(e) => setEditedTaskValue(e.target.value)}
+                />
+                <button onClick={saveEditedTask}>Save</button>
+              </>
+            ) : (
+              <>
+                {task.value}
+                <button onClick={() => startEditingTask(task.id, task.value)}>
+                  Update task
+                </button>
+                <button onClick={() => deleteTask(task.id)}>Delete task</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
